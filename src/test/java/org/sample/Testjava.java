2b0c72b51b89a89ba92b20de32573722df221896
rajesh191996 <rajesh.m.yadhav@gmail.com>
2025-04-20 20:57:25 +0530

    Fisrt commit

--- src/test/java/org/sample/Testjava.java
+++ src/test/java/org/sample/Testjava.java
@@ -1,5 +1,13 @@
 package org.sample;
 
 public class Testjava {
+	
+	//need to write test cases here
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Rajesh welcome to virtusa");
+		
+	}
 
 }
