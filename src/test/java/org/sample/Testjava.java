8ad0a9ce627cb2e0087ab97fb1cdc2aff0e5ea9d
rajesh191996 <rajesh.m.yadhav@gmail.com>
2025-04-20 21:03:06 +0530

    Second Commit

--- src/test/java/org/sample/Testjava.java
+++ src/test/java/org/sample/Testjava.java
@@ -1,12 +1,22 @@
 package org.sample;
 
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Testjava {
 	
 	//need to write test cases here
 	
 	public static void main(String[] args) {
 		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		System.out.println("Rajesh welcome to virtusa");
+		
+		
 		
 	}
 
